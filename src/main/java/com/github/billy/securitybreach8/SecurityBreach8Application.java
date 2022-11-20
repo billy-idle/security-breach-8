dd10171a1229b940a4cf91b9c5ce55f3d3cc7a9a
gguzmans <gguzmans@emeal.nttdata.com>
2022-11-20 14:07:58 -0500

    First commit!

---
+++ src/main/java/com/github/billy/securitybreach8/SecurityBreach8Application.java
@@ -0,0 +1,13 @@
+package com.github.billy.securitybreach8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBreach8Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SecurityBreach8Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/billy/securitybreach8/SecurityBreach8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.billy.securitybreach8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBreach8ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
